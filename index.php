6d440b42f7e5f6237a5f44eb63efa2065120e0de
Yannick Matongo <nay77127@gmail.com>
2023-11-01 17:48:31 +0100

    added button for adding invoices

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
         <!-- navbar -->
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#"><i class="fa-regular fa-user px-3"></i>Facturation</a>
+                <a class="navbar-brand fw-bold" href="#"><i class="fa-regular fa-user fw-bold px-3"></i>Facturation</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -47,24 +47,30 @@
     <section class="container py-5">
         <div class="row">
             <div class="col-lg-8 col-sm mb-5 mx-auto">
-                <h1 class="fs-4 text-center lead text-primary">
+                <h1 class="fs-4 text-center lead text-primary fw-bold">
                     FACTURATION
                 </h1>
             </div>
         </div>
-        <hr class="hr-light">
+        <!-- Divider -->
+        <hr class="hr">
+
         <div class="row">
             <div class="col-md-6">
                 <h5 class="fw-bold mb-0">Liste des factures</h5>
             </div>
             <div class="col-md-6">
                 <div class="d-flex justify-content-end">
-                    <button class="btn btn-primary btn-sm me-3"><i class="fas fa-folder-plus px-1"></i>Nouveau</button>
+                    <button class="btn btn-primary btn-sm me-3" data-bs-toggle="modal" data-bs-target="#createModal"><i class="fas fa-folder-plus px-1"></i>Nouveau</button>
                     <a href="#" class="btn btn-success btn-sm" id="export"><i class="fas fa-table px-1"></i>Exporter</a>
                 </div>
             </div>
         </div>
-        <hr class="hr-light">
+
+        <!-- Divider -->
+        <hr class="hr">
+
+        <!-- Table Facture -->
         <div class="row">
             <div class="table-responsive" id="orderTable">
                 <table class="table table-striped">
@@ -81,28 +87,96 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
+                        <?php for ($i = 0; $i < 100; $i++) : ?>
+                            <tr>
+                                <th scope="row"><?= $i ?></th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                                <td>@mdo</td>
+                                <td>
+                                    <a href="" class="text-info me-2 infoBtn" title="voir détails"><i class="fas fa-info-circle"></i></a>
+                                    <a href="" class="text-primary me-2 editBtn" title="modifier"><i class="fas fa-edit"></i></a>
+                                    <a href="" class="text-danger me-2 deleteBtn" title="supprimé"><i class="fas fa-trash-alt"></i></a>
+                                </td>
+                            </tr>
+                        <?php endfor; ?>
                     </tbody>
                 </table>
             </div>
         </div>
     </section>
 
+    <!-- Modal -->
+    <div class="modal fade" id="createModal" tabindex="-1" aria-labelledby="createModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="createModalLabel">Nouvelle Facture</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="" post="" id="formOrder">
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="costumer" name="customer">
+                            <label for="customer">Nom du client</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input type="text" class="form-control" id="costumer" name="cashier">
+                            <label for="cashier">Nom du caissier</label>
+                        </div>
+                        <div class="row g-2">
+                            <div class="col-md">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="costumer" name="amount">
+                                    <label for="amount">Montant</label>
+                                </div>
+                            </div>
+
+                            <div class="col-md">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="costumer" name="received">
+                                    <label for="received">Montant perçu</label>
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="costumer" name="received">
+                                    <label for="received">Montant perçu</label>
+                                </div>
+                            </div>
+                            <div class="col-md">
+                                <div class="form-floating">
+                                    <select name="form-select" id="state" aria-label="state" name="state">
+                                        <option value="facturée">Facturée</option>
+                                        <option value="Payée">Payée</option>
+                                        <option value="Annulée">Annulée</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button>
+                    <button type="button" class="btn btn-primary">Ajouter <i class="fas fa-plus"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <!-- Bootstrap Script -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-
     <!-- Datatable script -->
     <script src="https://cdn.datatables.net/v/bs5/dt-1.13.6/datatables.min.js"></script>
     <!-- sweet alert script -->
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <!-- Link Script -->
+    <script src="process.js"></script>
     <!-- Link Script -->
     <script src="process.js"></script>
 </body>
--- model.php
+++ model.php
@@ -1,1 +1,33 @@
 <?php
+
+class Database
+{
+    private $host = "mysql:dbname=crud_ajax";
+    private $user = "root";
+    private $pswd = "root";
+
+    private function getConnection()
+    {
+        // to recover database connection errors
+        try {
+            return new PDO($this->host, $this->user, $this->pswd);
+        } catch (PDOException $e) {
+            die('Erreur:' . $e->getMessage());
+        }
+    }
+
+    public function create(string $customer, string $cashier, int $amount, int $received, int $returned, string $state)
+    {
+        //write the request to send it to the server
+        $q = $this->getConnection()->prepare("INSERT INTO factures (:customer, :cashier, :amount, :received, :returned, :state) VALUEs (customer, cashier, amount, received , returned, state)");
+
+        return $q->execute([
+            'customer' => $customer,
+            'cashier' => $cashier,
+            'amount' => $amount,
+            'received' => $received,
+            'returned' => $returned,
+            'state' => $state
+        ]);
+    }
+}
